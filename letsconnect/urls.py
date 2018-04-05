65ddcb6aab9ee3b31d4b941fd1ea2bae49706d43
Deepak Sharma <sharma1725@gmail.com>
2018-04-05 18:33:30 +0530

    Angular Added

--- letsconnect/urls.py
+++ letsconnect/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from myapp.views import IndexView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', IndexView.as_view(), name="app_index"),
 ]
