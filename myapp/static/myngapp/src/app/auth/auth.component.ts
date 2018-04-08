2636c23ddfdc592d2028b13d2ec6a7e06a0390d9
Deepak Sharma <sharma1725@gmail.com>
2018-04-09 00:51:12 +0530

    fb login implemented

--- myapp/static/myngapp/src/app/auth/auth.component.ts
+++ myapp/static/myngapp/src/app/auth/auth.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+
+declare const FB: any;
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -8,8 +11,60 @@ import { Component, OnInit } from '@angular/core';
 export class AuthComponent implements OnInit {
 
   constructor() { }
+  FB:any;
 
   ngOnInit() {
+    FB.getLoginStatus((response) => {
+      this.statusChangeCallback(response);
+      console.log("YEah it worked");
+      
+    });
+  }
+
+  statusChangeCallback = (response) => {
+    console.log(response);
+    let authResponse = response;
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      console.log('Logged in.');
+      FB.api('/me', { fields: 'email,name,first_name,last_name' }, function (response) {
+        console.log(response);
+      });
+      console.log("AR",response.authResponse);
+      
+      FB.api(
+        `/${response.authResponse.userID}/groups`,
+        function (response) {
+          console.log("UserGroups", response);
+          
+          if (response && !response.error) {
+            /* handle the result */
+          }
+        }
+      );
+
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      // document.getElementById('status').innerHTML = 'Please log ' +
+      //     'into this app.';
+      console.log("not Authorised");
+
+    }
+  }
+
+
+  myFacebookLogin =  () => {
+    FB.login(function (response) {
+      if (response.status === 'connected') {
+        console.log("Person Logged in", response);
+
+        // Logged into your app and Facebook.
+      } else {
+        console.log("not Logged in", response);
+
+        // The person is not logged into this app or we are unable to tell. 
+      }
+    }, { scope: 'publish_actions, public_profile, email, user_managed_groups'  });
   }
 
 }
--- myapp/static/myngapp/src/assets/js/fb.js
+++ myapp/static/myngapp/src/assets/js/fb.js
@@ -1,30 +1,8 @@
 $(function() {
-    console.log("FB Initsa");
-    FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
-    });
     
-
+    FB.Event.subscribe('auth.login', function () {
+        console.log('FB Event Auth login');
+    });
 });
 
 
-function statusChangeCallback(response) {
-    console.log(response);
-    
-    if (response.status === 'connected') {
-        // Logged into your app and Facebook.
-        console.log('Logged in.');
-    } else if (response.status === 'not_authorized') {
-        // The person is logged into Facebook, but not your app.
-        // document.getElementById('status').innerHTML = 'Please log ' +
-        //     'into this app.';
-        console.log("not Authorised");
-        
-    }
-}  
-
-
-function myFacebookLogin() {
-    FB.login(function () { }, { scope: 'publish_actions' });
-    // this.FB.login();
-}
