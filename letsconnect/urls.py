8998311bd9d02e658bacb76af73117f8a89b7f32
Deepak Sharma <sharma1725@gmail.com>
2018-04-09 18:27:54 +0530

    Implementing Twitch from FB

--- letsconnect/urls.py
+++ letsconnect/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from myapp.views import IndexView
+from myapp.views import IndexView, LoginView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', IndexView.as_view(), name="app_index"),
+    url(r'^$', IndexView.as_view(), name="index"),
+    url(r'^login/$', LoginView.as_view(), name="login"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class IndexView(TemplateView):
+
+class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "myapp/index.html"
+
+
+class LoginView(TemplateView):
+    template_name = "myapp/login.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs['clientId'] = "hn9wdfmenh2m4g0d1zwenonvq4yexi"
+        kwargs['redirectUrl'] = "http://localhost:8001"
+        kwargs['scope'] = "viewing_activity_read+openid"
+        return kwargs
