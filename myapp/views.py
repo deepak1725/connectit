d554e81d54f2ce77921912a97b2e5b34f4813898
Deepak Sharma <sharma1725@gmail.com>
2018-04-10 22:31:38 +0530

    Obj Name Changes

--- myapp/views.py
+++ myapp/views.py
@@ -34,13 +34,14 @@ class AuthView(View):
                 'grant_type': 'authorization_code',
                 'redirect_uri': 'http://localhost:8001/auth',
             }
-            r = requests.post('https://id.twitch.tv/oauth2/token', data=payload)
+            accessTokenObj = requests.post('https://id.twitch.tv/oauth2/token', data=payload)
             # print("Json")
-            print(r.json())
+            print(accessTokenObj.json())
 
-            if r.status_code == 200:
+            if accessTokenObj.status_code == 200:
                 #Getting status of Token and UserId
-                r = r.json()
+
+                accessTokenObj = accessTokenObj.json()
                 url = 'https://api.twitch.tv/kraken/user'
                 headers = {
                     'Accept': 'application/vnd.twitchtv.v5+json',
@@ -48,6 +49,7 @@ class AuthView(View):
                     'Client-ID': "hn9wdfmenh2m4g0d1zwenonvq4yexi",
                 }
                 userObj = requests.get(url, headers=headers)
+
 
                 if userObj.status_code == 200:
                     userObj = userObj.json()
@@ -60,13 +62,12 @@ class AuthView(View):
                             "first_name": userObj["name"],
                             "is_active": userObj["email_verified"],
                         },
+
                         "socialModel" : {
                             "scope": r["scope"],
-                            "refresh_token": r["refresh_token"],
-                            "access_token": r["access_token"],
-                            "expires_in": r["expires_in"],
-
-
+                            "refresh_token": accessTokenObj["refresh_token"],
+                            "access_token": accessTokenObj["access_token"],
+                            "expires_in": accessTokenObj["expires_in"],
                             "email_notifications": userObj["notifications"]["email"],
                             "auth_id": userObj["_id"],
                             "updated_at": userObj["updated_at"],
