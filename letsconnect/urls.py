51a0bac3a9041a7b68e4be9f10f1a7104fe4467f
Deepak Sharma <sharma1725@gmail.com>
2018-04-21 10:58:12 +0530

    static files url added

--- letsconnect/urls.py
+++ letsconnect/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^', include('social_django.urls', namespace='social')),
     url(r'^', include(('myapp.urls','myapp'), namespace='myapp')),
     url(r'^admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
