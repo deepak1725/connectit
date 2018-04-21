c7ff5fb7eea59d1e97ed5a725939aa9c3dff218e
Deepak Sharma <sharma1725@gmail.com>
2018-04-21 09:32:41 +0530

    Heroku settings

--- letsconnect/settings.py
+++ letsconnect/settings.py
@@ -85,9 +85,16 @@ WSGI_APPLICATION = 'letsconnect.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'PORT': '5432',
     }
 }
 
--- letsconnect/urls.py
+++ letsconnect/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from myapp.views import IndexView, LoginView,UserDetailsView, FollowerCallbackView, StreamCallbackView
 
 urlpatterns = [
     url(r'^', include('social_django.urls', namespace='social')),
