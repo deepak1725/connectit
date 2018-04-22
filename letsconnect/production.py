4bc527a18d03fbd5783e3764636c5d7715c076b5
Deepak Sharma <sharma1725@gmail.com>
2018-04-22 07:34:59 +0530

    Heroku

--- letsconnect/production.py
+++ letsconnect/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c=p(%yei_hhz+ry002ptd#1dssimsyog^q+e8uh_l9&jyef+zy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', False)
 
 
 ALLOWED_HOSTS = ['*']
