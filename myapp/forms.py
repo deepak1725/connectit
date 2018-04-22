fc5436203a84c0d34dfb02ddae28e2c1a9c1f9b3
Deepak Sharma <sharma1725@gmail.com>
2018-04-22 12:03:28 +0530

    minor layout structure changes

--- myapp/forms.py
+++ myapp/forms.py
@@ -7,7 +7,7 @@ class SocialAuthForm(forms.Form, ModelForm):
     name = forms.CharField(label="Full name", widget= forms.TextInput( attrs= {'class': 'form-control'}))
     email = forms.EmailField(label="Email", widget= forms.EmailInput( attrs= {'class': 'form-control'}))
     mobile = forms.IntegerField(label="Phone number", required=False, widget= forms.TextInput( attrs= {'class': 'form-control'}))
-    email_notifications = forms.BooleanField(label="Get Notification", required=False, initial=True,widget=forms.CheckboxInput(attrs={'class':"custom-control-input"}))
+    email_notifications = forms.BooleanField(label="Get notification about change in stream state and new user follows", required=False, initial=True,widget=forms.CheckboxInput(attrs={'class':"custom-control-input"}))
 
     class Meta:
         model = SocialAuth
